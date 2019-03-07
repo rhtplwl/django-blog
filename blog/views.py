422eb51a17987e10c581cc015db720670edadb8e
rhtplwl <paliwal@iitj.ac.in>
2019-03-08 02:02:40 +0530

    Blog website with Django Python framework

---
+++ blog/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
+
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.filter(published_Date__lte=timezone.now()).order_by('published_Date')
+    return render(request, 'blog/index.html', {'posts': posts, 'nbar': "index"})
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_Date__lte=timezone.now()).order_by('published_Date')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'nbar':"post_list"})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_Date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form, 'nbar': "post_new"})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_Date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
